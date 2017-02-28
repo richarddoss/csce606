c2dbbd54b470a741adac63360a2540150fc733bb
Richard <richarddoss07195@gmail.com>
2017-02-28 19:30:31 +0000

    part 2 complete

--- rottenpotatoes-rails-intro/app/controllers/movies_controller.rb
+++ rottenpotatoes-rails-intro/app/controllers/movies_controller.rb
@@ -1,5 +1,6 @@
 class MoviesController < ApplicationController
-
+  
+  #params[:ratings]={"G"=>"1",'PG'=>"1",'PG-13'=>"1", "R"=>"1"}
   def movie_params
     params.require(:movie).permit(:title, :rating, :description, :release_date)
   end
@@ -10,21 +11,26 @@ class MoviesController < ApplicationController
     # will render app/views/movies/show.<extension> by default
   end
 
-  def index
-    @movies = Movie.all
-  end
-  def index
-    @movies = Movie.order(params[:sort])
-  end
-  def index
-    @movies = Movie.all
-    @movies= Movie.order(params[:sort_by])
-    if params[:sort_by] == 'title'
-      @title_header = 'hilite'
-    elsif params[:sort_by] == 'release_date'
-      @release_header ='hilite'
+  
+    def index
+      #  defaults = {:ratings=>["G"=>"1","PG"=>1,"PG-13"=>1,"R"=>"1"]}
+      #  params = defaults.merge(params)
+        @all_ratings=['G','PG','PG-13','R']
+        #params[:ratings]={"G"=>"1", "R"=>"1"}
+        #if params[:ratings].keys!=['G','PG','PG-13','R']
+      #  @movies=Movie.where("rating IN (?)", params[:ratings].keys).order(params[:sort_by])
+        #@movies=Movie.where("rating IN (?)", params[:ratings].keys).order(params[:sort_by])
+        @movies = Movie.order(params[:sort_by])
+        @movies = @movies.where("rating IN (?)", params[:ratings].keys) if params[:ratings].present?
+    
+        if params[:sort_by] == 'title'
+          @title_header = 'hilite'
+        elsif params[:sort_by] == 'release_date'
+          @release_header ='hilite'
+        end
     end
-  end
+    #end
+  
 
   def new
     # default: render 'new' template
