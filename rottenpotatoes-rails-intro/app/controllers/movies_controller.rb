0ddbdf60f8aeb6e455edee4a772488c5d814d3a0
Richard <richarddoss07195@gmail.com>
2017-03-01 07:00:08 +0000

    Final changes

--- rottenpotatoes-rails-intro/app/controllers/movies_controller.rb
+++ rottenpotatoes-rails-intro/app/controllers/movies_controller.rb
@@ -16,39 +16,22 @@ class MoviesController < ApplicationController
     #  defaults = {:ratings=>["G"=>"1","PG"=>1,"PG-13"=>1,"R"=>"1"]}
     #  params = defaults.merge(params)
     @all_ratings=['G','PG','PG-13','R']
-    #if params[:ratings].present? or params[:sort_by].present?
-    #  session[:ratings]=params[:ratings]
-    #  session[:sort_by]=params[:sort_by]
-    #end
+    #session.clear
     
-    #if session[:sort_by].present? && params[:sort_by].nil?
-    #  redirect_to movies_path(sort_by: session[:sort_by]) and return
-    #end
-    
-    #params[:ratings]={"G"=>"1", "R"=>"1"}
-    #if params[:ratings].keys!=['G','PG','PG-13','R']
-    #  @movies=Movie.where("rating IN (?)", params[:ratings].keys).order(params[:sort_by])
-    #@movies=Movie.where("rating IN (?)", params[:ratings].keys).order(params[:sort_by])
-    #@movies = Movie.order(session[:sort_by])
-    # @movies = @movies.where("rating IN (?)", session[:ratings].keys) if session[:ratings].present?
-    
-    # if session[:sort_by] == 'title'
-      # @title_header = 'hilite'
-    #elsif session[:sort_by] == 'release_date'
-      #  @release_header ='hilite'
-    #end
 
     if(params[:ratings].present?)
       session[:ratings] = params[:ratings]
       if (session[:sort_by].present? and (not params[:sort_by].present?) )
         @movies = Movie.order(session[:sort_by]).where("rating IN (?)", params[:ratings].keys)
       else 
+        session[:sort_by] = params[:sort_by]
         @movies = Movie.order(params[:sort_by]).where("rating IN (?)", params[:ratings].keys)
       end
     elsif(session[:ratings].present?)
       if (session[:sort_by].present? and (not params[:sort_by].present?) )
         @movies = Movie.order(session[:sort_by]).where("rating IN (?)", session[:ratings].keys)
-      else 
+      else
+        session[:sort_by] = params[:sort_by]
         @movies = Movie.order(params[:sort_by]).where("rating IN (?)", session[:ratings].keys)
       end
     elsif
@@ -56,10 +39,15 @@ class MoviesController < ApplicationController
       puts "absent"
     end
     
-    if params[:sort_by] == 'title'
+    if params[:sort_by] == 'title' 
       @title_header= 'hilite'
     elsif params[:sort_by] == 'release_date'
        @release_header ='hilite'
+    elsif session[:sort_by] == 'release_date'
+       @release_header ='hilite'
+    elsif session[:sort_by] == 'title'
+       @title_header ='hilite'
+       
     end
     
   end
