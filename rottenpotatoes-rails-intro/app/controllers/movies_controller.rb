f8723672314e8920afa24e695c6f19fba1479c46
Richard <richarddoss07195@gmail.com>
2017-03-01 05:49:41 +0000

    Part 3 complete

--- rottenpotatoes-rails-intro/app/controllers/movies_controller.rb
+++ rottenpotatoes-rails-intro/app/controllers/movies_controller.rb
@@ -12,24 +12,58 @@ class MoviesController < ApplicationController
   end
 
   
-    def index
-      #  defaults = {:ratings=>["G"=>"1","PG"=>1,"PG-13"=>1,"R"=>"1"]}
-      #  params = defaults.merge(params)
-        @all_ratings=['G','PG','PG-13','R']
-        #params[:ratings]={"G"=>"1", "R"=>"1"}
-        #if params[:ratings].keys!=['G','PG','PG-13','R']
-      #  @movies=Movie.where("rating IN (?)", params[:ratings].keys).order(params[:sort_by])
-        #@movies=Movie.where("rating IN (?)", params[:ratings].keys).order(params[:sort_by])
-        @movies = Movie.order(params[:sort_by])
-        @movies = @movies.where("rating IN (?)", params[:ratings].keys) if params[:ratings].present?
-    
-        if params[:sort_by] == 'title'
-          @title_header = 'hilite'
-        elsif params[:sort_by] == 'release_date'
-          @release_header ='hilite'
-        end
-    end
+  def index
+    #  defaults = {:ratings=>["G"=>"1","PG"=>1,"PG-13"=>1,"R"=>"1"]}
+    #  params = defaults.merge(params)
+    @all_ratings=['G','PG','PG-13','R']
+    #if params[:ratings].present? or params[:sort_by].present?
+    #  session[:ratings]=params[:ratings]
+    #  session[:sort_by]=params[:sort_by]
     #end
+    
+    #if session[:sort_by].present? && params[:sort_by].nil?
+    #  redirect_to movies_path(sort_by: session[:sort_by]) and return
+    #end
+    
+    #params[:ratings]={"G"=>"1", "R"=>"1"}
+    #if params[:ratings].keys!=['G','PG','PG-13','R']
+    #  @movies=Movie.where("rating IN (?)", params[:ratings].keys).order(params[:sort_by])
+    #@movies=Movie.where("rating IN (?)", params[:ratings].keys).order(params[:sort_by])
+    #@movies = Movie.order(session[:sort_by])
+    # @movies = @movies.where("rating IN (?)", session[:ratings].keys) if session[:ratings].present?
+    
+    # if session[:sort_by] == 'title'
+      # @title_header = 'hilite'
+    #elsif session[:sort_by] == 'release_date'
+      #  @release_header ='hilite'
+    #end
+
+    if(params[:ratings].present?)
+      session[:ratings] = params[:ratings]
+      if (session[:sort_by].present? and (not params[:sort_by].present?) )
+        @movies = Movie.order(session[:sort_by]).where("rating IN (?)", params[:ratings].keys)
+      else 
+        @movies = Movie.order(params[:sort_by]).where("rating IN (?)", params[:ratings].keys)
+      end
+    elsif(session[:ratings].present?)
+      if (session[:sort_by].present? and (not params[:sort_by].present?) )
+        @movies = Movie.order(session[:sort_by]).where("rating IN (?)", session[:ratings].keys)
+      else 
+        @movies = Movie.order(params[:sort_by]).where("rating IN (?)", session[:ratings].keys)
+      end
+    elsif
+      @movies = Movie.order(params[:sort_by])
+      puts "absent"
+    end
+    
+    if params[:sort_by] == 'title'
+      @title_header= 'hilite'
+    elsif params[:sort_by] == 'release_date'
+       @release_header ='hilite'
+    end
+    
+  end
+  #end
   
 
   def new
